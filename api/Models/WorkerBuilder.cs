00ab544b0644a391252b8afdf1cf2b73b462312f
Brian Lake <bnlake@gmail.com>
2023-05-25 14:24:08 -0500

    created a worker model builder added a seed data function to workerservice constructor

---
+++ api/Models/WorkerBuilder.cs
@@ -0,0 +1,24 @@
+namespace api.Models;
+
+public class WorkerBuilder
+{
+    private Worker worker;
+
+    public WorkerBuilder()
+    {
+        this.worker = new Worker { };
+    }
+    public Worker Build() { return worker; }
+
+    public WorkerBuilder AddId(Guid id)
+    {
+        worker.Id = id;
+        return this;
+    }
+
+    public WorkerBuilder AddName(string name)
+    {
+        worker.Name = name;
+        return this;
+    }
+}
--- api/Services/WorkerService.cs
+++ api/Services/WorkerService.cs
@@ -10,6 +10,7 @@ public class WorkerService
     public WorkerService(ILogger<WorkerService> logger)
     {
         this.Logger = logger;
+        this.RegisterSeedWorkers();
         Logger.LogDebug($"Worker Services dictionary instantiated with {workers.Count} workers");
     }
 
@@ -22,5 +23,15 @@ public class WorkerService
     public Task<IList<Worker>> GetAllAsync()
     {
         return Task.FromResult(workers.Values.ToList() as IList<Worker>);
+    }
+
+    private void RegisterSeedWorkers()
+    {
+        Guid guid = Guid.NewGuid();
+        this.workers.Add(guid, new WorkerBuilder().AddId(guid).AddName("Headache").Build());
+        guid = Guid.NewGuid();
+        this.workers.Add(guid, new WorkerBuilder().AddId(guid).AddName("Heart-Failure").Build());
+        guid = Guid.NewGuid();
+        this.workers.Add(guid, new WorkerBuilder().AddId(guid).AddName("Stress").Build());
     }
 }
